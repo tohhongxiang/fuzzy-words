09552693b0dd490b600a9ee4db06f28bcaa47f23
Toh Hong Xiang <hongxiang@ieplsg.com>
2020-01-30 15:57:29 +0800

    Updates

---
+++ precache-manifest.fc2abec3f5387f01aee9cc702b476170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887cb359ba4f733e89299d2aa4045e0f",
+    "url": "/fuzzy-words/index.html"
+  },
+  {
+    "revision": "e4a9c0698e09665dfa90",
+    "url": "/fuzzy-words/static/css/main.6282a1be.chunk.css"
+  },
+  {
+    "revision": "eeb517bd992f7b9421db",
+    "url": "/fuzzy-words/static/js/2.1e371784.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/fuzzy-words/static/js/2.1e371784.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4a9c0698e09665dfa90",
+    "url": "/fuzzy-words/static/js/main.d586f028.chunk.js"
+  },
+  {
+    "revision": "e98b59205ec97a1e6ca5",
+    "url": "/fuzzy-words/static/js/runtime-main.ca3ad55c.js"
+  }
+]);
